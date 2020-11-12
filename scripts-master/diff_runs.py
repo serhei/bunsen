d1052ca93618387537c2ec5b1c025f1edfa8e5fa
Serhei Makarov <me@serhei.io>
2020-11-12 18:04:42 -0500

    +diff_runs: fix 1st-order algorithm for comparing testruns
    Found a problem in the diffing algorithm which drops some comparisons,
    fixed by synthesizing PASS testcases when the testrun only has a series
    of FAIL subtest. Basically, any subtest not mentioned in the testrun
    explicitly is assumed to be a PASS now. This yields much clearer and
    more useful diffs.
    
    Rewrote the logic for clarity in general. It should still work well
    for repos built without consolidate_pass parsing which store separate
    PASS subtests as well as FAIL subtests.
    
    XXX The same rewrite must still be done for the 2nd-order diff algorithm.
    
    XXX Changing the subtest name on a FAIL testcase (e.g. when the
    subtest name contains program output) will yield a PASS->FAIL,FAIL->PASS
    pair. Some way of filtering out these cases is desirable.

--- scripts-master/diff_runs.py
+++ scripts-master/diff_runs.py
@@ -8,12 +8,15 @@ cmdline_args = [
     ('pretty', True, None, "pretty-print instead of showing JSON"),
 ]
 
-# TODOXXX Add filtering option as in diff_commits.
-fail_outcomes = {'FAIL','KFAIL','XFAIL','UNTESTED','UNSUPPORTED','ERROR'}
+# TODO: Add filtering option as in diff_commits.
+#fail_outcomes = {'FAIL','KFAIL','XFAIL','UNTESTED','UNSUPPORTED','ERROR'}
+fail_outcomes = {'FAIL','KFAIL','XFAIL','ERROR'}
+# <- Most likely PASS->UNTESTED is not interesting, FAIL->UNTESTED is.
 
 import sys
 from bunsen import Bunsen, Testrun
 
+import re
 from fnmatch import fnmatchcase
 from common.format_output import get_formatter
 
@@ -27,88 +30,174 @@ def assign_map(m, key, val, warn_duplicate=True):
         print("(1) EXISTING {}\n(2) NEW {}".format(m[key], val))
     m[key] = val
 
-def subtest_name(str):
+test_outcome_regex = re.compile(r"(K|I|X)?(PASS|FAIL|ERROR|UNTESTED|UNSUPPORTED|UNRESOLVED): (?P<content>.*)")
+
+def subtest_name(s):
     # XXX Modify if you need to strip away any extra notes from subtest_name.
-    return str
+    m = test_outcome_regex.match(s)
+    if m is not None:
+        s = m.group('content')
+    return s
 
-def diff_testcases(outdiff, baseline_testcases, latest_tc):
-    '''
-    Pairwise compare latest_tc with baseline_testcases
-    and append any differences to outdiff.
-    '''
-    for tc1 in baseline_testcases:
-        if latest_tc['outcome'] != tc1['outcome']:
-            tc2 = dict(latest_tc)
-            if 'subtest' not in tc2 and 'subtest' in tc1:
-                tc2['subtest'] = subtest_name(tc1['subtest'])
-            tc2['baseline_outcome'] = tc1['outcome']
-            if tc2['baseline_outcome'] in fail_outcomes and tc2['outcome'] in fail_outcomes:
-                 continue
-            if 'origin_sum' in tc1:
-                tc2['baseline_sum'] = tc1['origin_sum']
-            if 'origin_log' in tc1:
-                tc2['baseline_log'] = tc1['origin_log']
-            outdiff.testcases.append(tc2)
+def add_comparison(outdiffs, baseline_tc, latest_tc, assume_pass=False):
+    baseline_outcome = baseline_tc['outcome'] if baseline_tc is not None \
+        else "PASS" if assume_pass \
+        else "(none)"
+    latest_outcome = latest_tc['outcome'] if latest_tc is not None \
+        else "PASS" if assume_pass \
+        else "(none)"
+    if baseline_outcome == latest_outcome:
+        return
+    #print ("<p>BASELINE {}:: {}<br/>LATEST {}:: {}</p>".format(baseline_outcome, baseline_tc, latest_outcome, latest_tc))
+    if baseline_outcome in fail_outcomes and latest_outcome in fail_outcomes:
+        # XXX skip FAIL->FAIL type changes:
+        return
+    if baseline_outcome not in fail_outcomes and latest_outcome not in fail_outcomes:
+        # XXX skip PASS->PASS,PASS->UNTESTED type changes:
+        return
 
+    # simplify lookups
+    if baseline_tc is None: baseline_tc = {}
+    if latest_tc is None: latest_tc = {}
+
+    tc2 = dict(latest_tc)
+    if 'subtest' not in tc2 and 'subtest' in baseline_tc:
+        tc2['subtest'] = baseline_tc['subtest']
+    tc2['baseline_outcome'] = baseline_outcome
+    if 'origin_sum' in baseline_tc:
+        tc2['baseline_sum'] = baseline_tc['origin_sum']
+    if 'origin_log' in baseline_tc:
+        tc2['baseline_log'] = baseline_tc['origin_log']
+    append_map(outdiffs, tc2['name'], tc2)
+
+# TODOXXX replaces diff_testruns
 def diff_testruns(baseline, latest, key=None):
     '''
-    1st-order diff: testcases in latest that
-    differ from baseline.
+    1st-order diff: testcases in latest that differ from baseline.
     '''
     # XXX Only writes 'testcases' field -- caller decides how to diff metadata.
     diff = Testrun()
     diff['bunsen_commit_id'] = latest['bunsen_commit_id']
     diff['baseline_bunsen_commit_id'] = baseline['bunsen_commit_id']
+    diff_testcases = {} # name -> lst of tc comparisons (used to sort by exp)
 
     # XXX The logic on how to handle identically-named testcases can get tricky.
     #
-    # Supposing we have:
-    # - baseline@cur1 testcase.exp XFAIL: subtest1
-    # - baseline@cur2 testcase.exp FAIL: subtest1
-    # - latest@cur3 testcase.exp FAIL: subtest1
+    # 1. Some testcases have a subtest, others don't.
+    #    In particular, parsing logs with consolidate_pass option
+    #    will consolidate all PASS subtest into a single testcase entry.
     #
-    # We do pairwise comparisons of matching subtests in baseline and latest:
-    # - cur1->cur3 testcase.exp XFAIL->FAIL: subtest1
-    # - cur2->cur3 testcase.exp FAIL->FAIL: subtest1
+    #    In general, if a testcase name is present in the testrun,
+    #    we consider all its subtests not explicitly mentioned as PASS.
+    #    If a testcase name is not present in the testrun,
+    #    we consider all its subtests not explicitly mentioned as (none).
     #
-    # The second comparison (FAIL->FAIL) is dropped, leaving a single
-    # XFAIL->FAIL regression.
+    # 2. DejaGNU log files can report several testcases
+    #    with the same name and subtest. We don't consider changing
+    #    the number of times a testcase reports the same outcome
+    #    to be a significant difference.
     #
-    # Note this means merely changing the number of times a subtest
-    # reports failure is not considered a significant difference.
-    tc1_map = {} # name -> lst of testcase
-    tc1_name_map = {} # name -> lst of testcase (only if subtest missing)
-    tc1_subtest_map = {} # name+subtest -> lst of testcase
+    # 3. Do pairwise comparisons of matching subtests in baseline and latest:
+    #    - cur1->cur3 testcase.exp UNTESTED->FAIL: subtest1
+    #    - cur2->cur3 testcase.exp FAIL->FAIL: subtest1
+    #    In this example, the second comparison (FAIL->FAIL) is dropped,
+    #    leaving a single UNTESTED->FAIL regression.
+
+    tc1_entire_map = {} # name matching key -> tc with name but no subtest
+    tc1_subtest_map = {} # name matching key -> lst of tc with name and subtest
+    tc1_matching_map = {} # name+subtest -> lst of testcase
 
     for tc1 in baseline.testcases:
         name = tc1['name']
+        diff_testcases[name] = [] # XXX ensure sane order in final diff
         if key is not None and not fnmatchcase(name, key): continue
-        append_map(tc1_map, name, tc1)
         if 'subtest' in tc1:
+            append_map(tc1_subtest_map, name, tc1)
             name_plus_subtest = name + '+' + subtest_name(tc1['subtest'])
-            append_map(tc1_subtest_map, name_plus_subtest, tc1)
+            append_map(tc1_matching_map, name_plus_subtest, tc1)
         else:
-            append_map(tc1_name_map, name, tc1)
+            assign_map(tc1_entire_map, name, tc1)
 
+    # First compare latest testcases with name and subtest:
+    entire_names = set() # names of real or synthetic tcs
+    latest_entire = [] # real or synthetic tcs with name but no subtest
+    matched_subtests = set() # name+subtest already compared in tc2
     for tc2 in latest.testcases:
         name = tc2['name']
         if key is not None and not fnmatchcase(name, key): continue
-        name_plus_subtest = None
-        if 'subtest' in tc2:
-            name_plus_subtest = name + '+' + subtest_name(tc2['subtest'])
-        if name_plus_subtest is not None and \
-           name_plus_subtest in tc1_subtest_map:
-            diff_testcases(diff, tc1_subtest_map[name_plus_subtest], tc2)
-        elif name in tc1_name_map: # XXX no subtest
-            diff_testcases(diff, tc1_name_map[name], tc2)
-        elif name in tc1_map: # XXX subtest in tc1, no subtest in tc2
-            diff_testcases(diff, tc1_map[name], tc2)
-        else: # XXX tc2 has no equivalent in baseline, use None as baseline outcome
-            if tc2['outcome'] != None:
-                tc2 = dict(tc2)
-                tc2['baseline_outcome'] = None
-                diff.testcases.append(tc2)
 
+        if 'subtest' not in tc2:
+            latest_entire.append(tc2)
+            entire_names.add(name)
+            continue
+        if name not in entire_names:
+            entire_tc = {'name': name, 'outcome': 'PASS'}
+            latest_entire.append(entire_tc)
+            entire_names.add(name)
+
+        name_plus_subtest = name + '+' + subtest_name(tc2['subtest'])
+        matched_subtests.add(name_plus_subtest)
+
+        baseline_entire = tc1_entire_map[name] if name in tc1_entire_map \
+            else None
+        tc1_has_entire = baseline_entire is not None
+        baseline_subtests = tc1_subtest_map[name] if name in tc1_subtest_map \
+            else []
+        tc1_has_subtests = len(baseline_subtests) > 0
+        matching_subtests = []
+        if name_plus_subtest in tc1_matching_map:
+            matching_subtests = tc1_matching_map[name_plus_subtest]
+        tc1_has_matching = len(matching_subtests) > 0
+
+        if tc1_has_matching:
+            #print("<p>{} case1: matching subtests {}</p>".format(name, name_plus_subtest))
+            for tc1 in matching_subtests:
+                add_comparison(diff_testcases, tc1, tc2)
+                # XXX already added to matched_subtests above
+        if tc1_has_entire:
+            #print("<p>{} case2: baseline entire, latest subtest {}</p>".format(name, name_plus_subtest))
+            tc1 = baseline_entire; add_comparison(diff_testcases, tc1, tc2)
+        if not tc1_has_matching and not tc1_has_entire:
+            #print("<p>{} case3: baseline absent or non-matching subtest, latest subtest {}</p>".format(name, name_plus_subtest))
+            # XXX tc1 is absent or has non-matching subtests
+            assume_pass = tc1_has_subtests
+            add_comparison(diff_testcases, None, tc2, assume_pass=assume_pass)
+            #if tc1_has_subtests: print("<p>(baseline present, assuming subtest is PASS)</p>")
+
+    # Then compare latest testcases with name but not subtest:
+    for tc2 in latest_entire:
+        name = tc2['name']
+        #if key is not None and not fnmatchcase(name, key): continue
+
+        baseline_entire = tc1_entire_map[name] if name in tc1_entire_map \
+            else None
+        tc1_has_entire = baseline_entire is not None
+        baseline_subtests = tc1_subtest_map[name] if name in tc1_subtest_map \
+            else []
+        tc1_has_subtests = len(baseline_subtests) > 0
+        # TODO: Need better caching: for name_plus_subtest in tc1_subtest_names[name]
+        unmatched_subtests = []
+        for name_plus_subtest in tc1_matching_map:
+            if name_plus_subtest.startswith(name) \
+               and name_plus_subtest not in matched_subtests:
+                unmatched_subtests += tc1_matching_map[name_plus_subtest]
+        tc1_has_unmatched = len(unmatched_subtests) > 0
+
+        if tc1_has_entire:
+            #print("<p>{} case4: matching entire</p>".format(name))
+            tc1 = baseline_entire; add_comparison(diff_testcases, tc1, tc2)
+        if tc1_has_unmatched:
+            #print("<p>{} case5: baseline subtest, latest entire</p>".format(name))
+            for tc1 in unmatched_subtests:
+                add_comparison(diff_testcases, tc1, tc2)
+        if not tc1_has_entire and not tc1_has_unmatched:
+            #print("<p>{} case6: baseline absent, latest entire</p>".format(name))
+            assume_pass = tc1_has_subtests
+            add_comparison(diff_testcases, None, tc2, assume_pass=assume_pass)
+
+    # Then sort results by .exp:
+    for name, tcs in diff_testcases.items():
+        diff.testcases += tcs
     return diff
 
 def outcome_2or(tc):
@@ -153,6 +242,7 @@ def diff_2or_testcases(outdiff2, baseline_testcases, latest_tc):
             add_2or_origins(tc2, 'baseline_origins', tc1)
             outdiff2.testcases.append(tc2)
 
+# TODO: REWRITE CONSISTENTLY WITH diff_testcases
 def diff_2or(diff_baseline, diff_latest, key=None):
     '''
     2nd-order diff: changes in diff_latest that
