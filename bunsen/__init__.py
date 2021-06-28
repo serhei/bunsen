b3e819c648fc9cb0d4283bfee6939029b342028b
Serhei Makarov <me@serhei.io>
2021-06-28 11:11:31 -0400

    bunsen/__init__.py: remove no longer needed import hack

--- bunsen/__init__.py
+++ bunsen/__init__.py
@@ -12,14 +12,3 @@ from .model import Index, Testlog, Cursor, Testcase, Testrun
 from .repo import Workdir, Bunsen, BunsenOptions
 from .utils import BunsenError
 from .version import __version__
-
-# TODO: Temporary hack while moving declarations out of ../bunsen.py:
-# import sys, os
-# if os.path.basename(sys.argv[0]) != 'bunsen.py':
-#     sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-#     import importlib.util
-#     spec = importlib.util.spec_from_file_location("bunsen", os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'bunsen.py')))
-#     b = importlib.util.module_from_spec(spec)
-#     sys.modules['b2'] = b
-#     spec.loader.exec_module(b)
-#     from b2 import Bunsen
